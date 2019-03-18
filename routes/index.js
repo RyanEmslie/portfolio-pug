12f2d6a33262504778987bb26c86e5210f3d6e8d
ryan emslie <ryan.emslie@gmail.com>
2019-03-17 22:53:06 -0400

    reorderd page orders

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,27 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const data = require('../data.json').property;
+const data = require("../data.json").property;
 
 //Route for the "root" homepage
 //imports the entire json as variable
-router.get('/', (req, res) => {
-        res.render('index', { 
-                jsonData: data
-            });
-})
+// router.get("/", (req, res) => {
+//   res.render("index", {
+//     jsonData: data
+//   });
+// });
+
+//Route for the "root" homepage
+//imports the entire json as variable
+router.get("/portfolio", (req, res) => {
+  res.render("index", {
+    jsonData: data
+  });
+});
 
 //Route to the about page
-router.get('/about', (req, res) => {
-        res.render('about');
-})
+router.get("/", (req, res) => {
+  res.render("about");
+});
 
 //so index.js can be used in app.js
 module.exports = router;
